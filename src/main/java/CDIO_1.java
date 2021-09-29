88eb3379736d1e0c70c1930c38e7cbcde2bf6b43
Klakawaka <ahmadsandhu01@gmail.com>
2021-09-29 09:09:11 +0200

    rettelser :)

--- src/main/java/CDIO_1.java
+++ src/main/java/CDIO_1.java
@@ -5,21 +5,22 @@ public class CDIO_1 {
         GUI gui = new GUI();
         Scanner scan = new Scanner(System.in);
         Dices p1die1, p1die2;
-        int p1sum;
-        int p2sum;
+
         int totalsum1 = 0;
         int totalsum2 = 0;
+        int choice;
+
         p1die1 = new Dices();
         p1die2 = new Dices();
-// hej guys :)
-// hej Ahmad
+
         String player1;
         String player2;
-        int choice;
+
         System.out.println("Wellcome to CDIO game part 1 :)"); // Menu
         System.out.println("Press 1) to start the game");
         System.out.println("Press 2) to exit the game");
         choice = scan.nextInt();
+
         if (choice == 1){
             scan.nextLine();
             System.out.println("Enter the name for player 1"); // choice = 1: creating the players
@@ -28,22 +29,25 @@ public class CDIO_1 {
             System.out.println("Enter the name for player 2");
             player2 = scan.nextLine();
             System.out.println("Player 2 = " + player2);
+
             while(true){ // while loop and the game starts :)
+
                 int die1Facevalue = p1die1.roll();
                 int die2Facevalue = p1die2.roll();
-            System.out.println(player1 + "  Please press enter to roll the dices"); // player 1 turn
-            scan.nextLine();
-            p1die1.roll();
-            p1die2.roll();
-            //p1sum = p1die1.getFaceValue() + p1die2.getFaceValue();
-            int p1sum1 = die1Facevalue + die2Facevalue;
-            System.out.println(player1 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p1sum1);
-            gui.setDice(die1Facevalue,die2Facevalue);
-            totalsum1 = totalsum1 + p1sum1;
-            System.out.println(player1 + " totalsum " + totalsum1);
+                System.out.println(player1 + "  Please press enter to roll the dices"); // player 1 turn
+                scan.nextLine();
+                p1die1.roll();
+                p1die2.roll();
+                int p1sum1 = die1Facevalue + die2Facevalue;
+                System.out.println(player1 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p1sum1);
+                gui.setDice(die1Facevalue,die2Facevalue);
+                totalsum1 = totalsum1 + p1sum1;
+                System.out.println(player1 + " totalsum " + totalsum1);
+
              if (die1Facevalue == 6 && die2Facevalue == 6){
                     System.out.println(player1 + " WON!");
                     break;
+
                 }
              else if (die1Facevalue == 1 && die2Facevalue == 1){
                  totalsum1 = 0;
@@ -59,15 +63,14 @@ public class CDIO_1 {
                  else{
                      die1Facevalue = p1die1.roll();
                      die2Facevalue = p1die2.roll();
-                System.out.println(player1 + "  Please press enter to roll the dices");
-                scan.nextLine();
-                p1die1.roll();
-                p1die2.roll();
-                //p1sum = p1die1.getFaceValue() + p1die2.getFaceValue();
-                System.out.println(player1 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p1sum1);
+                     System.out.println(player1 + "  Please press enter to roll the dices");
+                     scan.nextLine();
+                     p1die1.roll();
+                     p1die2.roll();
+                     System.out.println(player1 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p1sum1);
                      gui.setDice(die1Facevalue,die2Facevalue);
-                 totalsum1 = totalsum1 + p1sum1;
-                 System.out.println(player1 + " totalsum " + totalsum1);
+                     totalsum1 = totalsum1 + p1sum1;
+                     System.out.println(player1 + " totalsum " + totalsum1);
                     }}
 
 
@@ -79,16 +82,16 @@ public class CDIO_1 {
                 scan.nextLine();
                  p1die1.roll();
                  p1die2.roll();
-                //p2sum = p1die1.getFaceValue() + p1die2.getFaceValue();
                 int p2sum1 = die1Facevalue + die2Facevalue;
                 System.out.println(player2 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p2sum1);
-                 gui.setDice(die1Facevalue,die2Facevalue);
-                 totalsum2 = totalsum2 + p2sum1;
-                 System.out.println(player2 + " totalsum " + totalsum2);
+                gui.setDice(die1Facevalue,die2Facevalue);
+                totalsum2 = totalsum2 + p2sum1;
+                System.out.println(player2 + " totalsum " + totalsum2);
             }
             if (die1Facevalue == 6 && die2Facevalue == 6){
                     System.out.println(player2 + " WON!");
                     break;
+
                 }
             else if (die1Facevalue == 1 && die2Facevalue == 1){
                     totalsum2 = 0;
@@ -102,16 +105,15 @@ public class CDIO_1 {
                         else{
                             die1Facevalue = p1die1.roll();
                             die2Facevalue = p1die2.roll();
-                        System.out.println(player2 + "  Please press enter to roll the dices");
-                        scan.nextLine();
-                        p1die1.roll();
-                        p1die2.roll();
-                        //p2sum = p1die1.getFaceValue() + p1die2.getFaceValue();
+                            System.out.println(player2 + "  Please press enter to roll the dices");
+                            scan.nextLine();
+                            p1die1.roll();
+                            p1die2.roll();
                             int p2sum1 = die1Facevalue + die2Facevalue;
-                        System.out.println(player2 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p2sum1);
+                            System.out.println(player2 + "  Got " + die1Facevalue + " and " + die2Facevalue +" which gave " + p2sum1);
                             gui.setDice(die1Facevalue,die2Facevalue);
-                        totalsum2 = totalsum2 + p2sum1;
-                        System.out.println(player2 + " totalsum " + totalsum2);
+                            totalsum2 = totalsum2 + p2sum1;
+                            System.out.println(player2 + " totalsum " + totalsum2);
                          }
             }
 
